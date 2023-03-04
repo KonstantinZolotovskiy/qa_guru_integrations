b07a799b358e3f700fea5055de61b5676b0bf861
Konstantin Zolotovskiy <konstantin.zolotovskiy@brandmaker.com>
2023-03-04 17:03:32 +0700

    add WebDriverConfig

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,19 +1,17 @@
 package tests;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.WebDriverProvider;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.JavaAdvancedPage;
 import pages.JavaPage;
 import pages.PythonPage;
 import testdata.TestData;
 
-import static com.codeborne.selenide.Configuration.*;
-import static com.codeborne.selenide.Configuration.browserCapabilities;
 
 public class TestBase {
 
@@ -24,16 +22,7 @@ public class TestBase {
 
     @BeforeAll
     public static void setUp() {
-        baseUrl = "https://qa.guru";
-        browser = System.getProperty("browser", "chrome");
-        browserVersion = System.getProperty("browserVersion", "100.0");
-        browserSize = System.getProperty("browserSize", "1920x1080");
-        remote = "https://user1:1234@" + System.getProperty("remote", "selenoid.autotests.cloud") + "/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        browserCapabilities = capabilities;
+        WebDriverProvider.setConfig();
     }
 
     @BeforeEach
