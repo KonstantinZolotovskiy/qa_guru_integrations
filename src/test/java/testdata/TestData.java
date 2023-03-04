98de7214241e53e56765b1cdf6ae6afded97f4b7
Konstantin Zolotovskiy <konstantin.zolotovskiy@brandmaker.com>
2023-03-04 16:58:44 +0700

    fix TestData

--- src/test/java/testdata/TestData.java
+++ src/test/java/testdata/TestData.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class TestData {
     public String
-            firstOptionPrice = "25 200",
-            secondOptionPrice = "37 300",
-            thirdOptionPrice = "54 100";
+            firstOptionPrice = "23 800",
+            secondOptionPrice = "35 300",
+            thirdOptionPrice = "51 150";
 
     public List<String> javaLessonTitleList = new ArrayList<>();
 
@@ -16,35 +16,33 @@ public class TestData {
         javaLessonTitleList.add("2. Git. Github. Погружаемся. Дмитрий Тучс");
         javaLessonTitleList.add("Дополнительное занятие. Профессионально работаем с IntelliJ IDEA Юрий Артамонов (JetBrains)");
         javaLessonTitleList.add("3. Погружаемся в инструментарий и библиотеки. Станислав Васенков и Дмитрий Тучс.");
-        javaLessonTitleList.add("4. Selenide #1. Занятие в записи. Алексей Виноградов");
-        javaLessonTitleList.add("Дополнительное занятие. Selenide #1. Алексей Виноградов");
-        javaLessonTitleList.add("Дополнительное занятие. Selenide IDE – и такое бывает");
-        javaLessonTitleList.add("5. Основы Java. Дмитрий Тучс.");
-        javaLessonTitleList.add("6. Продолжаем разрабатывать автотесты. PageObjects. Станислав Васенков.");
-        javaLessonTitleList.add("7. Продолжаем разрабатывать автотесты. Генерация тестовых данных. Станислав Васенков.");
+        javaLessonTitleList.add("4. Selenide #1. Алексей Виноградов");
+        javaLessonTitleList.add("5. Selenide #2. Алексей Виноградов. Занятие в записи");
+        javaLessonTitleList.add("6. Основы Java. Дмитрий Тучс.");
+        javaLessonTitleList.add("Консультация к занятиям Selenide. Алексей Виноградов");
+        javaLessonTitleList.add("7. Продолжаем разрабатывать автотесты. PageObjects. Станислав Васенков.");
+        javaLessonTitleList.add("8. Продолжаем разрабатывать автотесты. Генерация тестовых данных. Станислав Васенков.");
         javaLessonTitleList.add("Дополнительное занятие. Основы Java. Часть II. Дмитрий Тучс");
-        javaLessonTitleList.add("8. JUnit 5. Дмитрий Тучс");
-        javaLessonTitleList.add("9. Allure Reports. Артём Ерошенко. Занятие в записи");
-        javaLessonTitleList.add("Дополнительное занятие. Основы Java. Часть III. Практика. Дмитрий Тучс");
+        javaLessonTitleList.add("9. JUnit 5. Дмитрий Тучс");
         javaLessonTitleList.add("10. Работаем с файлами. Дмитрий Тучс");
-        javaLessonTitleList.add("11. Jenkins. Создаем первую задачу. Станислав Васенков.");
-        javaLessonTitleList.add("12. Управляем параметрами в коде и в Jenkins. Станислав Васенков.");
-        javaLessonTitleList.add("13. Telegram-бот. Отправляем уведомления о результатах прохождении автотестов. Станислав Васенков.");
-        javaLessonTitleList.add("14. Учимся быстро разрабатывать готовые проекты для тестовых заданий. Станислав Васенков.");
-        javaLessonTitleList.add("15. Selenoid. Роман Орлов");
-        javaLessonTitleList.add("16. Selenide #2. Алексей Виноградов");
-        javaLessonTitleList.add("17. REST API. Пишем автотесты с REST Assured. Станислав Васенков.");
-        javaLessonTitleList.add("18. Библиотека Owner. Занятие в записи. Артём Ерошенко");
+        javaLessonTitleList.add("11. Allure Reports. Артём Ерошенко. Занятие в записи");
+        javaLessonTitleList.add("Дополнительное занятие. Основы Java. Часть III. Практика. Дмитрий Тучс");
+        javaLessonTitleList.add("13. Управляем параметрами в коде и в Jenkins. Станислав Васенков.");
+        javaLessonTitleList.add("14. Telegram-бот. Отправляем уведомления о результатах прохождении автотестов. Станислав Васенков.");
+        javaLessonTitleList.add("15. Учимся быстро разрабатывать готовые проекты для тестовых заданий. Станислав Васенков.");
+        javaLessonTitleList.add("16. Selenoid. Роман Орлов");
+        javaLessonTitleList.add("17. Библиотека Owner. Занятие в записи. Артём Ерошенко");
+        javaLessonTitleList.add("18. REST API. Пишем автотесты с REST Assured. Станислав Васенков.");
         javaLessonTitleList.add("19. REST API. Модели. Спецификации. Станислав Васенков.");
-        javaLessonTitleList.add("20. REST API. Декомпозируем UI-тесты. Подключаем отчетность");
-        javaLessonTitleList.add("Дополнительное занятие. REST API / Groovy. Алексей Кузнецов");
-        javaLessonTitleList.add("Дополнительное занятие. REST API. Закрепляющее занятие. Дмитрий Тучс");
-        javaLessonTitleList.add("Дополнительное занятие. SOAP API. Дмитрий Тучс");
+        javaLessonTitleList.add("Дополнительное занятие в записи. REST API / Groovy. Алексей Кузнецов");
+        javaLessonTitleList.add("20. REST API. Декомпозируем UI-тесты. Станислав Васенков");
+        javaLessonTitleList.add("Дополнительное занятие в записи. SOAP API. Дмитрий Тучс");
+        javaLessonTitleList.add("Дополнительное занятие в записи. REST API. Закрепляющее занятие. Дмитрий Тучс");
         javaLessonTitleList.add("21. Мобильная автоматизация #1. Разрабатываем автотесты с Browserstack");
         javaLessonTitleList.add("22. Мобильная автоматизация #2. Разрабатываем автотесты с эмулятором Android-устройства и на своем телефоне");
         javaLessonTitleList.add("23. Allure TestOps. Артем Ерошенко. Занятие в записи");
         javaLessonTitleList.add("24. Дипломный проект");
-        javaLessonTitleList.add("25. Переезд на собственную инфраструктуру с ресурсов школы.  Егор Иванов (выпускник первого потока)");
+        javaLessonTitleList.add("25. Переезд на собственную инфраструктуру с ресурсов школы. Егор Иванов");
     }
 
     public List<String> javaAdvanceLessonTitleList = new ArrayList<>();
@@ -60,8 +58,8 @@ public class TestData {
         javaAdvanceLessonTitleList.add("7. Параллелизация тестов в JUnit5: Как не выстрелить себе в ногу. Дмитрий Тучс");
         javaAdvanceLessonTitleList.add("8. Кастомизация фреймворков – расширяем Allure, Selenide, HTTP клиенты. Дмитрий Тучс");
         javaAdvanceLessonTitleList.add("9. Нативное тестирование iOS. XCTest. Часть 3. Владислав Зингер");
-        javaAdvanceLessonTitleList.add("10. Unit тесты – пишем тестируемый код и тесты для него. Дмитрий Тучс");
-        javaAdvanceLessonTitleList.add("11. Создаем WEB-сервис на Java. Дмитрий Тучс");
+        javaAdvanceLessonTitleList.add("10. Создаем WEB-сервис на Java. Дмитрий Тучс");
+        javaAdvanceLessonTitleList.add("11. Unit тесты – пишем тестируемый код и тесты для него. Дмитрий Тучс");
         javaAdvanceLessonTitleList.add("12. Тестируем SOAP сервисы с помощью REST Assured. Дмитрий Тучс");
         javaAdvanceLessonTitleList.add("13. Тестируем GraphQL сервисы с помощью REST Assured. Дмитрий Тучс");
         javaAdvanceLessonTitleList.add("14. Тестируем gRPC сервисы с помощью нативных библиотек. Дмитрий Тучс");
@@ -83,17 +81,17 @@ public class TestData {
         pythonLessonTitleList.add("Разбор домашнего задания к уроку «Selene #1». Яков Крамаренко");
         pythonLessonTitleList.add("8. PageObjects. Часть I. Модульная парадигма для построения модели для тестов. Яков Крамаренко");
         pythonLessonTitleList.add("9. Allure Reports. Артём Ерошенко. Занятие в записи");
-        pythonLessonTitleList.add("Разбор домашнего задания к уроку «Модульная парадигма для построения модели для тестов». Яков Крамаренко.");
         pythonLessonTitleList.add("10. Jenkins. Создаем первую задачу. Васенков Станислав");
+        pythonLessonTitleList.add("Разбор домашнего задания к уроку «Модульная парадигма для построения модели для тестов». Яков Крамаренко.");
         pythonLessonTitleList.add("11. Телеграм-бот. Отправляем уведомления о результатах прохождении автотестов. Васенков Станислав");
-        pythonLessonTitleList.add("12. PageObjects. Часть II. Объектно-ориентированная парадигма для построения модели для тестов. Яков Крамаренко");
+        pythonLessonTitleList.add("12. Учимся быстро разрабатывать готовые проекты для тестовых заданий. Васенков Станислав");
         pythonLessonTitleList.add("13. Pytest. Часть I. Сергей Хомутинин");
+        pythonLessonTitleList.add("14. PageObjects. Часть II. Объектно-ориентированная парадигма для построения модели для тестов. Яков Крамаренко");
+        pythonLessonTitleList.add("15. Pytest. Часть II. Сергей Хомутинин");
         pythonLessonTitleList.add("Разбор домашнего задания к уроку «Объектно-ориентированная парадигма для построения модели для тестов». Яков Крамаренко");
-        pythonLessonTitleList.add("14. Selenoid. Роман Орлов");
-        pythonLessonTitleList.add("15. REST API. Часть I. Александр Котляр");
-        pythonLessonTitleList.add("16. Pytest. Часть II. Сергей Хомутинин");
-        pythonLessonTitleList.add("17. Учимся быстро разрабатывать готовые проекты для тестовых заданий. Васенков Станислав");
-        pythonLessonTitleList.add("18. Venv, Poetry и управление зависимостями проекта. Сергей Хомутинин");
+        pythonLessonTitleList.add("16. Selenoid. Роман Орлов");
+        pythonLessonTitleList.add("17. Venv, Poetry и управление зависимостями проекта. Сергей Хомутинин");
+        pythonLessonTitleList.add("18. REST API. Часть I. Александр Котляр");
         pythonLessonTitleList.add("19. REST API. Часть II. Александр Котляр");
         pythonLessonTitleList.add("20. REST API. Часть III. Александр Котляр");
         pythonLessonTitleList.add("21. REST API. Часть IV. Александр Котляр");
